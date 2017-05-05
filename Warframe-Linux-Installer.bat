9955784831986aac4c75af3cd4291833c8144f50
gloriouseggroll <gloriouseggroll@gmail.com>
2017-05-04 21:45:04 -0400

    fix paths for installer

--- Warframe-Linux-Installer.bat
+++ Warframe-Linux-Installer.bat
@@ -1,5 +1,6 @@
 echo off
 title Warframe Command Line Install tool
-start /wait C:\Warframe\Downloaded\Public\Drivers\DirectX9\DXSETUP.exe /silent
-start /wait C:\Warframe\Downloaded\Public\Warframe-updater.exe -log:/Preprocess.log -dx10:0 -dx11:0 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
+set gameDir=C:\Program Files\Warframe\Downloaded\Public
+start /wait "%gameDir%\Drivers\DirectX9\DXSETUP.exe" /silent
+start /wait "%gameDir%\Warframe-updater.exe" -log:/Preprocess.log -dx10:0 -dx11:0 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
 exit 0
