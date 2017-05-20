95c4fb8d6b8a94a3fa3eb6fd973c30b5ae65bbe4
GloriousEggroll <gloriouseggroll@gmail.com>
2017-05-19 21:59:07 -0400

    Fix launcher not properly replacing itself

--- Warframe-Launcher.bat
+++ Warframe-Launcher.bat
@@ -6,6 +6,7 @@ set gameDir=C:\Program Files\Warframe\Downloaded\Public
 start /wait "%gameDir%\Warframe-updater.exe" -log:/Preprocess.log -dx10:0 -dx11:0 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
 del "%gameDir%\Warframe.exe"
 if exist "%launcherDir%\Launcher.exe" (
+del "%launcherDir%\Launcher-copy.exe"
 move /wait /Y "%launcherDir%\Launcher.exe" "%launcherDir%\Launcher-copy.exe"
 ) else (
 start /wait Warframe.msi
