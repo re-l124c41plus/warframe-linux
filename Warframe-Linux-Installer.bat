effb3409cd166cb4158e8005b8df201da9acb2df
GloriousEggroll <gloriouseggroll@gmail.com>
2017-07-29 21:53:59 -0400

    change to work with wine > 2.10

--- Warframe-Linux-Installer.bat
+++ Warframe-Linux-Installer.bat
@@ -1,6 +1,6 @@
 echo off
 title Warframe Command Line Install tool
 set gameDir=C:\Program Files\Warframe\Downloaded\Public
-start /wait "%gameDir%\Drivers\DirectX9\DXSETUP.exe" /silent
-start /wait "%gameDir%\Warframe-updater.exe" -log:/Preprocess.log -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
+cmd /C "%gameDir%\Drivers\DirectX9\DXSETUP.exe /silent"
+cmd /C "%gameDir%\Warframe-updater.exe -log:/Preprocess.log -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate"
 exit 0
