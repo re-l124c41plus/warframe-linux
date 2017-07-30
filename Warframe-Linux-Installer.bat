16a70b082e59033f8df64c6f9e4f6beb648a7e73
GloriousEggroll <gloriouseggroll@gmail.com>
2017-07-29 22:06:17 -0400

    fix for global location

--- Warframe-Linux-Installer.bat
+++ Warframe-Linux-Installer.bat
@@ -1,6 +1,6 @@
 echo off
 title Warframe Command Line Install tool
-set gameDir=C:\Program Files\Warframe\Downloaded\Public
+set gameDir=%cd%\Downloaded\Public
 cmd /C "%gameDir%\Drivers\DirectX9\DXSETUP.exe /silent"
 cmd /C "%gameDir%\Warframe-updater.exe -log:/Preprocess.log -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate"
 exit 0
