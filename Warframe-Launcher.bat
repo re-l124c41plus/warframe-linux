50498c92fa9c61f46df89d491571a8721f402dfc
GloriousEggroll <gloriouseggroll@gmail.com>
2017-07-29 20:49:50 -0400

    attempt to fix compatibility with wine > 2.10 and use global settings

--- Warframe-Launcher.bat
+++ Warframe-Launcher.bat
@@ -1,21 +1,39 @@
-echo off
+
 title Warframe Linux Game Launcher
+
 set launcherDir=C:\users\%USERNAME%\Local Settings\Application Data\Warframe\Downloaded\Public\Tools
-set gameDir=C:\Program Files\Warframe\Downloaded\Public
+set gameDir=%cd%\Downloaded\Public
+set currDir=%cd%
 
-start /wait "%gameDir%\Warframe-updater.exe" -log:/Preprocess.log -dx10:0 -dx11:0 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
+cmd /C "%gameDir%\Warframe-updater.exe" -log:/Preprocess.log -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
+
+if exist "%currDir%\Warframe.msi" (
+del "%currDir%\Warframe.msi"
+)
+
+if exist "%gameDir%\Warframe.exe" (
 del "%gameDir%\Warframe.exe"
-if exist "%launcherDir%\Launcher-copy.exe" (
-del "%launcherDir%\Launcher-copy.exe"
-)
-else if exist "%launcherDir%\Launcher.exe" (
-move /wait /Y "%launcherDir%\Launcher.exe" "%launcherDir%\Launcher-copy.exe"
-) else (
-start /wait Warframe.msi
-move /wait /Y "%launcherDir%\Launcher.exe" "%launcherDir%\Launcher-copy.exe"
 )
 
-start "%launcherDir%\Launcher-copy.exe"
-ping -n 10 127.0.0.1 > nul
+if exist "%gameDir%\Warframe.x64.exe" (
+del "%gameDir%\Warframe.x64.exe"
+)
+
+if exist "%launcherDir%\Launcher.exe" (
+del "%launcherDir%\Launcher.exe"
+)
+
+wget.exe /transfer "Warframe" "http://content.warframe.com/dl/Warframe.msi" "Warframe.msi"
+ping -n 5 0.0.0.0 > nul
+
+msiexec /i Warframe.msi
+ping -n 5 0.0.0.0 > nul
+
+if exist "%gameDir%\Warframe.exe" (
 xcopy /Y "%gameDir%\Warframe.exe" "%gameDir%\Warframe-updater.exe"
+)
+if exist "%gameDir%\Warframe.x64.exe" (
+xcopy /Y "%gameDir%\Warframe.x64.exe" "%gameDir%\Warframe-updater.exe"
+)
+
 exit 0
