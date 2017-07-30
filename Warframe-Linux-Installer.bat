667b87de7c89989f48d360efd3e2aee9afe5a979
GloriousEggroll <gloriouseggroll@gmail.com>
2017-07-29 23:19:16 -0400

    remove, not needed

--- Warframe-Linux-Installer.bat
+++
@@ -1,6 +0,0 @@
-echo off
-title Warframe Command Line Install tool
-set gameDir=%cd%\Downloaded\Public
-start /wait "%gameDir%\Drivers\DirectX9\DXSETUP.exe" /silent
-start /wait "%gameDir%\Warframe-updater.exe" -log:/Preprocess.log -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
-exit 0
