eadbcedc867fcc454be875c47a8665eb857cbcb5
GloriousEggroll <gloriouseggroll@gmail.com>
2017-07-29 20:50:30 -0400

    allow dx10 and dx11 updates

--- Warframe-Linux-Installer.bat
+++ Warframe-Linux-Installer.bat
@@ -2,5 +2,5 @@ echo off
 title Warframe Command Line Install tool
 set gameDir=C:\Program Files\Warframe\Downloaded\Public
 start /wait "%gameDir%\Drivers\DirectX9\DXSETUP.exe" /silent
-start /wait "%gameDir%\Warframe-updater.exe" -log:/Preprocess.log -dx10:0 -dx11:0 -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
+start /wait "%gameDir%\Warframe-updater.exe" -log:/Preprocess.log -threadedworker:1 -cluster:public -language:en -applet:/EE/Types/Framework/ContentUpdate
 exit 0
