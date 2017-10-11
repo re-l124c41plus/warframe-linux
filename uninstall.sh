a5791971cc00f013603723442e66550f0ebd3abc
GloriousEggroll <gloriouseggroll@gmail.com>
2017-10-10 23:04:44 -0400

    add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "*************************************************"
+echo "The next few steps will remove all shortcuts and warframe files."
+echo "If root is required, please enter your root password when prompted."
+echo "*************************************************"
+
+echo "*************************************************"
+echo "Removing /usr/bin/warframe"
+echo "*************************************************"
+sudo rm -R /usr/bin/warframe
+
+echo "*************************************************"
+echo "Removing /usr/share/pixmaps/warframe.png"
+echo "*************************************************"
+sudo rm -R /usr/share/pixmaps/warframe.png
+
+echo "*************************************************"
+echo "Removing /usr/share/applications/warframe.desktop"
+echo "*************************************************"
+sudo rm -R /usr/share/applications/warframe.desktop 
+
+echo "*************************************************"
+echo "Removing sudo rm -R /home/$USER/Desktop/warframe.desktop"
+echo "*************************************************"
+sudo rm -R /home/$USER/Desktop/warframe.desktop
+
+echo "*************************************************"
+echo "Removing home/$USER/Warframe"
+echo "*************************************************"
+sudo rm -R home/$USER/Warframe
+
+echo "*************************************************"
+echo "Warframe has been successfully removed."
+echo "*************************************************"
