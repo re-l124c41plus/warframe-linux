aec1b8dc2429b7afa8f5af4c71af61b42f7ff429
mynamewastaken <mynamewastaken+git@gmail.com>
2017-10-22 16:00:12 -0500

    Change game installation directory into a variable
    Gives the end-user the easy option to change the installation directory for Warframe, as well as helps cut down on possible copy/paste errors during development.

--- install.sh
+++ install.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
+
+# Change to your preferred installation directory
+GAMEDIR="/home/${USER}/Warframe"
+
 echo "*************************************************"
 echo "Creating wine prefix and performing winetricks."
 echo "*************************************************"
 
-WINEDEBUG=-all WINEARCH=win32 WINEPREFIX=/home/$USER/Warframe winetricks -q vcrun2015 xact xinput win7
+WINEDEBUG=-all WINEARCH=win32 WINEPREFIX=$GAMEDIR winetricks -q vcrun2015 xact xinput win7
 
 echo "*************************************************"
 echo "Creating warframe directories."
 echo "*************************************************"
-mkdir -p /home/$USER/Warframe/drive_c/Program\ Files/Warframe/
-mkdir -p /home/$USER/Warframe/drive_c/users/$USER/Local\ Settings/Application\ Data/Warframe
+mkdir -p ${GAMEDIR}/drive_c/Program\ Files/Warframe/
+mkdir -p ${GAMEDIR}/drive_c/users/${USER}/Local\ Settings/Application\ Data/Warframe
 
 echo "*************************************************"
 echo "Copying warframe files."
 echo "*************************************************"
-cp -R * /home/$USER/Warframe/drive_c/Program\ Files/Warframe/ 
+cp -R * ${GAMEDIR}/drive_c/Program\ Files/Warframe/ 
 
-cd /home/$USER/Warframe/drive_c/Program\ Files/Warframe/
+cd ${GAMEDIR}/drive_c/Program\ Files/Warframe/
 chmod a+x wget.exe
-mv EE.cfg /home/$USER/Warframe/drive_c/users/$USER/Local\ Settings/Application\ Data/Warframe/EE.cfg
+mv EE.cfg ${GAMEDIR}/drive_c/users/${USER}/Local\ Settings/Application\ Data/Warframe/EE.cfg
 
 echo "*************************************************"
 echo "Applying warframe wine prefix registry settings."
 echo "*************************************************"
-WINEDEBUG=-all WINEARCH=win32 WINEPREFIX=/home/$USER/Warframe wine regedit /S wf.reg
+WINEDEBUG=-all WINEARCH=win32 WINEPREFIX=$GAMEDIR wine regedit /S wf.reg
 
 
 echo "*************************************************"
@@ -32,7 +36,7 @@ echo "*************************************************"
 
 echo "*************************************************"
 echo "Creating warframe shell script"
-echo "This script uses __GL_SHADER_DISK_CACHE=1 and __GL_SHADER_DISK_CACHE_PATH=/home/$USER/Warframe/"
+echo "This script uses __GL_SHADER_DISK_CACHE=1 and __GL_SHADER_DISK_CACHE_PATH=${GAMEDIR}"
 echo "If you are not installing the game on an SSD hard drive, please remove these two environment variables from /usr/bin/warframe"
 echo "You can do this via sudo nano /usr/bin/warframe"
 echo "*************************************************"
@@ -42,11 +46,11 @@ echo "#!/bin/bash" > warframe.sh
 echo "export PULSE_LATENCY_MSEC=60" >> warframe.sh
 echo "export __GL_THREADED_OPTIMIZATIONS=1" >> warframe.sh
 
-echo "cd /home/$USER/Warframe/drive_c/Program\ Files/Warframe/" >> warframe.sh
-echo "WINEARCH=win32 WINEPREFIX=/home/$USER/Warframe WINEDEBUG=-all wine cmd /C Warframe-Launcher.bat" >> warframe.sh
+echo "cd ${GAMEDIR}/drive_c/Program\ Files/Warframe/" >> warframe.sh
+echo "WINEARCH=win32 WINEPREFIX=$GAMEDIR WINEDEBUG=-all wine cmd /C Warframe-Launcher.bat" >> warframe.sh
 
 chmod a+x warframe.sh
-sudo cp /home/$USER/Warframe/drive_c/Program\ Files/Warframe/warframe.sh /usr/bin/warframe
+sudo cp ${GAMEDIR}/drive_c/Program\ Files/Warframe/warframe.sh /usr/bin/warframe
 
 
 read -p "Would you like a menu shortcut? y/n" -n 1 -r
